1ad40db675ca6b69bd5a863e372f4bb2a55f72a7
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-06-28 16:36:29 -0500

    Done.

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 class Circle extends React.Component {
 
-  
+  shouldComponentUpdate(nextProps) {
+    return nextProps.color !== this.props.color ? true : false
+  }
 
   render() {
     const { color } = this.props;
